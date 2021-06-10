1a7e9cc42dbc3b9ce7cac3f5e7d89713ebb5af04
Alan Jenkins <a.jenkins@synalogik.com>
2021-06-11 00:10:15 +0100

    Initial version of config handling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::borrow::Borrow;
 use std::default::Default;
+use std::fs::File;
+use std::io::prelude::*;
 use std::io;
+use std::process;
 
 use rusoto_core::Region;
 use rusoto_organizations::{Account, Organizations, OrganizationsClient, ListAccountsRequest};
 
-async fn generate_aws_config(region: &str, output_type: &str, sso_start_url: &str, sso_region: &str, sso_role_name: &str, accounts_list: &Vec<Account>) -> io::Result<String> {
-    let mut config_string: String = format!("[default] \nregion={}\noutput={}\n\n", &region, &output_type);
+async fn generate_aws_config(default_region: &str, default_output_type: &str, sso_start_url: &str, sso_region: &str, sso_role_name: &str, accounts_list: &Vec<Account>) -> io::Result<String> {
+    let mut config_string: String = format!("[default] \nregion={}\noutput={}\n\n", &default_region, &default_output_type);
 
     for account in accounts_list {
         let mut account_name: String;
@@ -36,8 +39,8 @@ async fn generate_aws_config(region: &str, output_type: &str, sso_start_url: &st
             &account_name,
             &sso_start_url,
             &sso_region,
-            &region,
-            &output_type,
+            &default_region,
+            &default_output_type,
             &account_id,
             &sso_role_name
         );
@@ -47,8 +50,80 @@ async fn generate_aws_config(region: &str, output_type: &str, sso_start_url: &st
     Ok(config_string)
 }
 
+fn find_config() -> Result<std::path::PathBuf, &'static str> {
+    let mut config_paths = vec![std::path::PathBuf::from("config.toml")];
+    let config_path: std::path::PathBuf;
+    match dirs::config_dir() {
+        Some(confdir) => {
+            let pos_config_path = confdir.join("aws-config-generator/config.toml");
+            config_paths.push(pos_config_path);
+        },
+        _ => {}
+    }
+    // *TODO* Implement config file finding code!
+    for check_config_path in config_paths {
+        if check_config_path.exists() {
+            config_path = std::path::PathBuf::from(check_config_path);
+            return Ok(config_path)
+        }
+    }
+
+    return Err("Config file not found")
+}
+
+fn read_config(config_path: &std::path::PathBuf) -> Result<String, std::io::Error> {
+    let mut file = File::open(config_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+fn parse_config(config_string: &String) -> Result<Box <toml::Value>, Box<String>> {
+    let config = match config_string.parse::<toml::Value>() {
+        Ok(parsed) => Box::new(parsed),
+        Err(err) => {
+            return Err(Box::new(format!("{}", err)))
+        }
+    };
+
+    Ok(config)
+}
+
+
+fn get_config() -> Box<toml::Value> {
+    let config_path = match find_config() {
+        Ok(config_path) => config_path,
+        Err(err) => {
+            eprintln!("Unable to find config file: {}", err);
+            process::exit(1);
+        }
+    };
+
+    let config_content = match read_config(&config_path) {
+        Ok(content) => content,
+        Err(err) => {
+            eprintln!("Unable to read the config file: {}", err);
+            process::exit(1);
+        }
+    };
+
+    let config = match parse_config(&config_content) {
+        Ok(config) => config,
+        Err(err) => {
+            eprintln!("Unable to parse the config file: {}", err);
+            process::exit(1);
+        },
+    };
+
+    config
+}
+
 #[tokio::main]
 async fn main() -> () {
+    let config = get_config();
+
+    println!("{:?}", config);
+
     let org_client = OrganizationsClient::new(Region::default());
     let list_accounts_input: ListAccountsRequest = Default::default();
 
@@ -57,11 +132,11 @@ async fn main() -> () {
             match output.accounts {
                 Some(accounts_list) => {
                     let config_string = generate_aws_config(
-                        "eu-west-2",
-                        "json",
-                        "https://synalogik-sso.awsapps.com/start",
-                        "eu-west-2",
-                        "AdministratorAccess",
+                        config["aws_cli_options"]["default_region"].as_str().unwrap(),
+                        config["aws_cli_options"]["default_output_type"].as_str().unwrap(),
+                        config["sso_options"]["sso_url"].as_str().unwrap(),
+                        config["sso_options"]["sso_region"].as_str().unwrap(),
+                        config["sso_options"]["sso_role"].as_str().unwrap(),
                         &accounts_list,
                     ).await;
                     println!("{}", config_string.unwrap());
@@ -74,4 +149,5 @@ async fn main() -> () {
             println!("Error: {:?}", error);
         }
     }
+    ()
 }
