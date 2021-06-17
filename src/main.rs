a24b99f197464be0a1dfbb629566353fbfedc6dd
Alan Jenkins <a.jenkins@synalogik.com>
2021-06-17 19:29:29 +0100

    Handle List account pagination

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::default::Default;
 
 use rusoto_core::Region;
-use rusoto_organizations::{ListAccountsRequest, Organizations, OrganizationsClient};
+use rusoto_organizations::{Account, ListAccountsRequest, Organizations, OrganizationsClient};
 use rusoto_sts::{GetCallerIdentityRequest, Sts, StsClient};
 
 mod configgen;
@@ -13,7 +13,6 @@ async fn main() -> () {
     println!("{:?}", config);
 
     let org_client = OrganizationsClient::new(Region::default());
-    let list_accounts_input: ListAccountsRequest = Default::default();
 
     let sts_client = StsClient::new(Region::default());
     let get_caller_identity_input = GetCallerIdentityRequest {};
@@ -27,32 +26,63 @@ async fn main() -> () {
         }
     };
 
-    match org_client.list_accounts(list_accounts_input).await {
+    let mut accounts_list = Vec::<Account>::new();
+    let mut next_token: Option<String> = Some("".to_string());
+    loop {
+        let mut list_accounts_input: ListAccountsRequest = Default::default();
+        println!("Next token is: '{}'\n", next_token.clone().unwrap());
+        if next_token.clone().unwrap() != String::from("") {
+            list_accounts_input.next_token = next_token.clone();
+        }
 
-        Ok(output) => match output.accounts {
-            Some(accounts_list) => {
-                let config_string = configgen::generate::generate_aws_config(
-                    &org_main_account.account.unwrap(),
-                    config["aws_cli_options"]["default_region"]
-                        .as_str()
-                        .unwrap(),
-                    config["aws_cli_options"]["default_output_type"]
-                        .as_str()
-                        .unwrap(),
-                    config["sso_options"]["sso_url"].as_str().unwrap(),
-                    config["sso_options"]["sso_region"].as_str().unwrap(),
-                    config["sso_options"]["sso_role"].as_str().unwrap(),
-                    &accounts_list,
-                )
-                .await;
-                println!("{}", config_string.unwrap());
+        match org_client.list_accounts(list_accounts_input).await {
+            Ok(output) => {
+                next_token = match output.next_token {
+                    Some(token) => Some(token),
+                    None => Some(String::from(""))
+                };
+
+                match output.accounts {
+                    Some(mut resp_accounts) => accounts_list.append(&mut resp_accounts),
+                    None => {},
+                }
+
+                if next_token.clone().unwrap() == String::from("") {
+                    println!("No more organisation account pages.");
+                    break;
+                } else {
+                    println!("There are more organisation account pages... getting the next");
+                }
+    //             match output.accounts {
+    //                 Some(resp_accounts_list) => {
+    // 
+    //                 }
+    // 
+    //                 None => {
+    //                     println!("No accounts in organization"),
+    //                 }
+    //             }
+            },
+            Err(error) => {
+                println!("Error: {:?}", error);
             }
-
-            None => println!("No accounts in organization"),
-        },
-        Err(error) => {
-            println!("Error: {:?}", error);
         }
     }
+
+    let config_string = configgen::generate::generate_aws_config(
+        &org_main_account.account.unwrap(),
+        config["aws_cli_options"]["default_region"]
+            .as_str()
+            .unwrap(),
+        config["aws_cli_options"]["default_output_type"]
+            .as_str()
+            .unwrap(),
+        config["sso_options"]["sso_url"].as_str().unwrap(),
+        config["sso_options"]["sso_region"].as_str().unwrap(),
+        config["sso_options"]["sso_role"].as_str().unwrap(),
+        &accounts_list,
+    )
+    .await;
+    println!("{}", config_string.unwrap());
     ()
 }
