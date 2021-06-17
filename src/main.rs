0ae155f0f075ef6f79aa21df18affb78e49c480e
Alan Jenkins <a.jenkins@synalogik.com>
2021-06-17 22:40:31 +0100

    Initial command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod configgen;
 
 #[tokio::main]
 async fn main() -> () {
+    let args = configgen::arg_parsing::get_args().await;
     let config = configgen::config::get_config();
 
     println!("{:?}", config);
